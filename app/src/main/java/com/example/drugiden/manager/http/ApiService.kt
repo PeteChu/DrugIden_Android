d437b272d471c13c29ec4aabef2473033e160721
schecterza <phuengton.ch.57@ubu.ac.th>
2017-11-15 18:48:55 +0700

    add advance search

--- app/src/main/java/com/example/drugiden/manager/http/ApiService.kt
+++ app/src/main/java/com/example/drugiden/manager/http/ApiService.kt
@@ -13,4 +13,25 @@ interface ApiService {
     @GET("/drugs/search/")
     fun quickSearch(@Query("input") input: String): Call<QuickSearchMedResponse>
 
+    @GET("/drugs/advance/")
+    fun advanceSearch(@Query("drtype") drtype: String
+                      , @Query("dtype") dtype: String
+                      , @Query("manufacturer") manufacturer: String
+                      , @Query("license") license: String
+                      , @Query("tradename") tradename: String
+                      , @Query("dsize") dsize: String
+                      , @Query("distributor") distributor: String
+                      , @Query("dstatus") dstatus: String
+                      , @Query("color1") color1: String
+                      , @Query("color2") color2: String
+                      , @Query("color3") color3: String
+                      , @Query("color4") color4: String
+                      , @Query("dshape") dshape: String
+                      , @Query("dgroup") dgroup: String
+                      , @Query("dshape1") dshape1: String
+                      , @Query("dshape2") dshape2: String
+                      , @Query("dshape3") dshape3: String
+                      , @Query("dshape4") dshape4: String
+    ): Call<String>
+
 }
